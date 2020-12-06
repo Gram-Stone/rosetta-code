87728617e7643d8a335ed73c9df91d24f2fedd18
Gram Stone <stonegram@gmail.com>
2020-12-06 17:41:28 -0500

    Create gen-fizz-buzz.js

---
+++ js/gen-fizz-buzz.js
@@ -0,0 +1,9 @@
+function genFizzBuzz(rules, num) {
+    var myArray = [];
+    for (var i = 0; i < rules.length; i++) {
+        if (num % rules[i][0] == 0){
+            myArray.push(rules[i][1])
+        }
+    }
+    return (myArray.length == 0) ? num.toString() : myArray.join("");
+}
