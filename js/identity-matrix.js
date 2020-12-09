80ded5f58676189aafb395169e536104450071ab
Gram Stone <stonegram@gmail.com>
2020-12-08 23:30:18 -0500

    Update identity-matrix.js

--- js/identity-matrix.js
+++ js/identity-matrix.js
@@ -1,8 +1,8 @@
 function idMatrix(n) {
     var matrix = [];
-    for (var i = 1; i <= n; i++) {
+    for (var i = 0; i < n; i++) {
         var row = [];
-        for (var j = 1; j <= n; j++) {
+        for (var j = 0; j < n; j++) {
             (i == j) ? row.push(1) : row.push(0);
         }
         matrix.push(row);
